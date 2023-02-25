903f1e524300ed9b888187cb1ee9320c2c6c7156
Mike McMillan <firefox1986@gmail.com>
2023-02-25 13:44:31 +0000

    Add first pass

---
+++ popup/password_gen.js
@@ -0,0 +1,80 @@
+function getRandomArrayEntry(array) {
+    return array[Math.floor(Math.random() * array.length)]
+}
+function getRandomEntries(array, length, separator = "") {
+    return Array.from(
+        { length: length },
+        (v, k) => getRandomArrayEntry(array)
+    );
+}
+function joinEntries(entries, separator) {
+    return entries.join(separator);
+}
+
+document.querySelector("#btnGenerateString").addEventListener("click", (e) => {
+    function getRandomString(requiredLength, includeNumbers, includeLowercase, includeUppercase, includeCharacters, includeExtendedAscii) {
+        const lowercase = "abcdefghijklmnopqrstuvwxyz";
+        const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const numbers = "0123456789";
+        const characters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ";
+        const extAscii = "€‚ƒ„…†‡ˆ‰Š‹ŒŽ‘’“”•–—˜™š›œžŸ¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ";
+        let chars =
+            (includeNumbers ? numbers : "") +
+            (includeLowercase ? lowercase : "") +
+            (includeUppercase ? uppercase : "") +
+            (includeCharacters ? characters : "") +
+            (includeExtendedAscii ? extAscii : "");
+        var entries = getRandomEntries(chars, requiredLength);
+        return joinEntries(entries, "");
+    }
+
+    document.querySelector("#generated-password").innerHTML = getRandomString(
+        document.getElementById("numPasswordLength").value,
+        document.getElementById("chkNumbers").checked,
+        document.getElementById("chkLowercase").checked,
+        document.getElementById("chkUppercase").checked,
+        document.getElementById("chkSymbols").checked,
+        document.getElementById("chkExtendedAscii").checked);
+});
+
+
+document.querySelector("#btnGenerateWords").addEventListener("click", (e) => {
+    function getRandomWords(numWords) {
+        var words = getWords();
+        var entries = getRandomEntries(words, numWords);
+        if (document.getElementById("chkCapitaliseFirstCharacter").checked) {
+            entries = entries.map((entry) => entry.charAt(0).toUpperCase() + entry.slice(1));
+        }
+        var separator = document.getElementById("txtSeparator").value;
+        if (!separator) {
+            separator = ".";
+        }
+        return joinEntries(entries, separator);
+    }
+    document.querySelector("#generated-password").innerHTML = getRandomWords(
+        document.getElementById("numWords").value
+    );
+});
+
+
+document.querySelector("#btnCopy").addEventListener("click", (e) => {
+    var element = document.getElementById("generated-password");
+    var text = element.innerHTML;
+    navigator.clipboard.writeText(text);
+});
+
+
+document.querySelector("#btnRandomWordsTab").addEventListener("click", (e) => {
+    document.getElementById("generateRandomWords").classList.remove('hidden');
+    document.getElementById("generateRandomString").classList.add('hidden');
+    document.getElementById("btnRandomWordsTab").classList.add('activeTab');
+    document.getElementById("btnRandomStringTab").classList.remove('activeTab');
+});
+document.querySelector("#btnRandomStringTab").addEventListener("click", (e) => {
+    document.getElementById("generateRandomString").classList.remove('hidden');
+    document.getElementById("generateRandomWords").classList.add('hidden');
+    document.getElementById("btnRandomStringTab").classList.add('activeTab');
+    document.getElementById("btnRandomWordsTab").classList.remove('activeTab');
+});
+
+document.querySelector("#btnGenerateWords").click();
